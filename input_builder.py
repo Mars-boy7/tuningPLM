82bb7226303ce4b8b24ca3f1669b41f80032dd40
Mars-boy7 <zjc0923@gmail.com>
2024-04-26 22:16:49 +0800

    change relation to rule

---
+++ input_builder.py
@@ -0,0 +1,183 @@
+class PromptBuilder:
+    def __init__(self) -> None:
+        pass
+    
+    def build_support_prompt_inputs(
+        self, 
+        token_list: list,
+        head_list: list,
+        tail_list: list,
+        relation_list: list,
+        pid2name: dict,
+        mask_token: str,
+        rule_file,#记得在方法调用给定规则文件的URL
+    ):
+        relation_name_list = [pid2name[r][0] for r in relation_list]
+        #添加转换代码
+        rule_list = map_rel_rule(rule_file,relation_name_list)
+        prompt_input_list = []
+        text_list = [' '.join(token) for token in token_list]
+        for i in range(len(text_list)):
+            #修改relation为rule
+            prompt_input = f"{text_list[i]} {head_list[i]} {rule_list[i]} {mask_token} {rule_list[i]} {tail_list[i]}"
+            prompt_input_list.append(prompt_input)
+        return prompt_input_list
+
+    #根据关系名称获取对应的规则
+    def map_rel_rule(rule_file, relation_name_list):
+        # 创建一个空的列表来存储匹配的规则
+        rule_list = []
+        
+        # 打开规则文件并逐行读取
+        with open(rule_file, "r") as file:
+            next(file)  # 跳过头行
+            for line in file:
+                # 移除换行符并按逗号分割
+                parts = line.strip().split(',')
+                # 将字符串表示的列表转换回列表
+                rule = eval(parts[0])
+                label = parts[1].strip()
+
+                # 检查当前行的标签是否在提供的关系名称列表中
+                if label in relation_name_list:
+                    # 如果在，则将整个规则添加到规则列表中
+                    rule_list.append(rule)
+        
+        return rule_list
+
+
+    #未使用的代码块
+    # def build_query_prompt_inputs(
+    #     self, 
+    #     token_list: list,
+    #     head_list: list,
+    #     tail_list: list,
+    #     mask_token: str,
+    # ):
+    #     prompt_input_list = []
+    #     text_list = [' '.join(token) for token in token_list]
+    #     for i in range(len(text_list)):
+    #         prompt_input = f"{text_list[i]} {head_list[i]} {mask_token} {tail_list[i]}"
+    #         prompt_input_list.append(prompt_input)
+    #     return prompt_input_list
+
+
+class EnsemblePormptBuilder(PromptBuilder):
+    def __init__(self) -> None:
+        super().__init__()
+    
+    def build_support_prompt_inputs(
+        self, 
+        token_list: list, 
+        head_list: list, 
+        tail_list: list, 
+        relation_list: list, 
+        pid2name: dict, 
+        mask_token: str,
+        rule_file,#记得在方法调用给定规则文件的URL
+    ):
+        relation_name_list = [pid2name[r][0] for r in relation_list]
+        #添加转换代码
+        rule_list = map_rel_rule(rule_file,relation_name_list)
+        prompt_input_list = []
+        text_list = [' '.join(token) for token in token_list]
+        for i in range(len(text_list)):
+            #修改relation为rule
+            prompt_input_1 = f"{text_list[i]} {rule_list[i]} {mask_token} {rule_list[i]} {head_list[i]}  {tail_list[i]}"
+            prompt_input_2 = f"{text_list[i]} {head_list[i]} {rule_list[i]} {mask_token} {rule_list[i]} {tail_list[i]}"
+            prompt_input_3 = f"{text_list[i]} {head_list[i]} {tail_list[i]} {rule_list[i]} {mask_token} {rule_list[i]}"
+            prompt_input_list.extend([prompt_input_1, prompt_input_2, prompt_input_3])
+        return prompt_input_list
+
+
+class EntityMarkerBuilder:
+    def __init__(self) -> None:
+        pass
+    
+    def mark(
+        self,
+        token_list: list,
+        head_idx_list: list,
+        tail_idx_list: list,
+        head_marker_start: str = '<subj>',
+        head_marker_end: str = '</subj>',
+        tail_marker_start: str = '<obj>',
+        tail_marker_end: str = '</obj>',
+    ):
+        new_token_list = []
+        for i in range(len(token_list)):
+            tokens = token_list[i]
+            h_idx = head_idx_list[i]
+            t_idx = tail_idx_list[i]
+            
+            h_start, h_end = self._get_start_end(h_idx)
+            t_start, t_end = self._get_start_end(t_idx)
+            
+            new_tokens = self._add_marker_to_tokens(
+                    h_marker_start=head_marker_start,
+                    h_marker_end=head_marker_end,
+                    t_marker_start=tail_marker_start,
+                    t_marker_end=tail_marker_end,
+                    tokens=tokens,
+                    h_idx_start=h_start,
+                    h_idx_end=h_end,
+                    t_idx_start=t_start,
+                    t_idx_end=t_end
+                )
+            new_token_list.append(new_tokens)
+        
+        return new_token_list
+    
+    def _get_start_end(self, idx: list):
+        # if len(idx) <= 1:
+        #     idx_start = idx[0]
+        #     idx_end = idx[0]
+        # else:
+        idx_start = idx[0]
+        idx_end = idx[-1]
+        return idx_start, idx_end
+    
+    def _add_marker_to_tokens(
+        self, 
+        h_marker_start: str,
+        h_marker_end: str,
+        t_marker_start: str,
+        t_marker_end: str,
+        tokens: list,
+        h_idx_start: int,
+        h_idx_end: int,
+        t_idx_start: int,
+        t_idx_end: int,
+    ):  
+        if h_idx_start > t_idx_start:
+            h_start = tokens[:h_idx_start] + [h_marker_start]
+            h_end = h_start + tokens[h_idx_start:h_idx_end+1] + [h_marker_end]
+            t_start = h_end + tokens[h_idx_end+1:t_idx_start] + [t_marker_start]
+            t_end = t_start + tokens[t_idx_start:t_idx_end+1] + [t_marker_end]
+            new_tokens = t_end + tokens[t_idx_end+1:]
+        else:
+            t_start = tokens[:t_idx_start] + [t_marker_start]
+            t_end = t_start + tokens[t_idx_start:t_idx_end+1] + [t_marker_end]
+            h_start = t_end + tokens[t_idx_end+1:h_idx_start] + [h_marker_start]
+            h_end = h_start + tokens[h_idx_start:h_idx_end+1] + [h_marker_end]
+            new_tokens = h_end + tokens[h_idx_end+1:]
+        return new_tokens
+
+
+class InputFeatureBuilder:
+    def __init__(self) -> None:
+        pass
+    
+    def build_input_feature(
+        self,
+        tokenizer,
+        text_list: list,
+        max_len: int = 256
+    ):
+        return tokenizer(
+            text=text_list,
+            padding='max_length',
+            truncation=True,
+            max_length=max_len,
+            return_tensors='pt'
+        )
---
+++ train.py
@@ -0,0 +1,300 @@
+from sampler import RelationSampler, ExampleSampler
+from input_builder import PromptBuilder, EntityMarkerBuilder, InputFeatureBuilder
+from transformers import PreTrainedTokenizer
+from model.proto import ProtoFSRE_Entity
+import torch
+import numpy as np
+import random
+import time
+
+
+class ProtoEntityTrainer:
+    def __init__(
+        self,
+        train_data: dict,
+        val_data: dict,
+        pid2name:dict,
+        tokenizer: PreTrainedTokenizer,
+        train_N: int,
+        train_K: int,
+        train_q: int,
+        epoch: int,
+        batch: int,
+        val_N: int,
+        val_K: int,
+        val_q: int,
+        val_batch: int,
+    ) -> None:
+        self.pid2name = pid2name
+        self.tokenizer = tokenizer
+        self.special_token_ids = self._init_tokenizer()
+        
+        self.train_data = train_data
+        self.train_N = train_N
+        self.train_K = train_K
+        self.train_q = train_q
+        self.rs_train = RelationSampler(
+            FewRel_data=self.train_data,
+            N=self.train_N
+        )
+        self.es_train = ExampleSampler(
+            FewRel_data=self.train_data,
+            K=self.train_K,
+            q=self.train_q
+        )
+        self.epoch = epoch
+        self.batch = batch
+        
+        self.val_data = val_data
+        self.val_N = val_N
+        self.val_K = val_K
+        self.val_q = val_q
+        self.val_batch = val_batch
+        self.rs_val = RelationSampler(
+            FewRel_data=self.val_data,
+            N=self.val_N
+        )
+        self.es_val = ExampleSampler(
+            FewRel_data=self.val_data,
+            K=self.val_K,
+            q=self.val_q
+        )
+        
+        self.entity_marker = EntityMarkerBuilder()
+        self.input_feature_builder = InputFeatureBuilder()
+
+    
+    def _init_tokenizer(self):
+        special_tokens_dict = {
+            'marker_start_head': '<subj>',
+            'marker_end_head': '</subj>',
+            'marker_start_tail': '<obj>',
+            'marker_start_tail': '</obj>',
+        }
+        self.tokenizer.add_special_tokens({"additional_special_tokens": ['<subj>', '</subj>', '<obj>', '</obj>']},)
+        special_token_ids_dict = {
+            '<subj>': self.tokenizer.convert_tokens_to_ids("<subj>"),
+            '</subj>': self.tokenizer.convert_tokens_to_ids("</subj>"),
+            '<obj>': self.tokenizer.convert_tokens_to_ids("<obj>"),
+            '</obj>': self.tokenizer.convert_tokens_to_ids("</obj>")
+        }
+        return special_token_ids_dict
+    
+    def train(
+        self,
+        model: ProtoFSRE_Entity,
+        loss_fn,
+        optimizer,
+        path_save_model: str
+    ):
+        encoder = model.get_encoder()
+        encoder.resize_token_embeddings(len(self.tokenizer))
+        eval_acc_best = [0, 0]
+        for t in range(self.epoch):
+            self.tprint(f"Epoch: {t + 1} / {self.epoch}, start training!")
+            self.train_loop(
+                entity_marker=self.entity_marker,
+                input_feature_builder=self.input_feature_builder,
+                model=model,
+                loss_fn=loss_fn,
+                optimizer=optimizer,
+            )
+            
+            self.tprint(f"Start eval!")
+            eval_acc = self.eval_loop(
+                model=model,
+                loss_fn=loss_fn
+            )
+            if (eval_acc > eval_acc_best[0]) or (eval_acc > eval_acc_best[1]):
+                self.tprint("Get A Best Model! Save It!")
+                model_filename = f"{t + 1}-{eval_acc:.4f}.pt"
+                torch.save(model.state_dict(), path_save_model / model_filename)
+                if eval_acc > eval_acc_best[0]:
+                    eval_acc_best[0] = eval_acc
+                elif eval_acc > eval_acc_best[1]:
+                    eval_acc_best[1] = eval_acc
+
+    
+    def eval_loop(
+        self,
+        model: ProtoFSRE_Entity,
+        loss_fn,
+    ):
+        model.eval()
+        val_loss, val_acc = 0, 0
+        with torch.no_grad():
+            for _ in range(self.val_batch):
+                val_input = self._build_episode_input(
+                    relation_sampler=self.rs_val,
+                    example_sampler=self.es_val,
+                    entity_marker=self.entity_marker,
+                    input_feature_builder=self.input_feature_builder
+                )
+                preds = model(
+                    support_prompt_inputs=val_input['prompt_input']['support'],
+                    query_prompt_inputs=val_input['prompt_input']['query'],
+                    support_entity_marker_inputs=val_input['marker_input']['support'],
+                    query_entity_marker_inputs=val_input['marker_input']['query'],
+                    mask_token_id=self.tokenizer.mask_token_id, 
+                    head_start_token_id=self.special_token_ids["<subj>"],
+                    head_end_token_id=self.special_token_ids["</subj>"],
+                    tail_start_token_id=self.special_token_ids["<obj>"],
+                    tail_end_token_id=self.special_token_ids["</obj>"],
+                    K=self.val_K
+                )
+                targets = torch.tensor(
+                    self.label_mapping(
+                        sampled_relation=val_input['sampled_relation'],
+                        targets=val_input['sampled_query_set']['label']
+                    )
+                )
+                loss = loss_fn(preds, targets)
+                val_loss += loss
+                val_acc += self.get_acc(
+                    preds=preds,
+                    targets=targets,
+                    N=self.val_N,
+                    q=self.val_q
+                )
+            val_loss /= self.val_batch
+            val_acc /= self.val_batch
+            self.tprint(f"Eval Avg Acc: {val_acc:.5f}, Eval Avg Loss: {val_loss.item():.7f}")
+            return val_acc
+        
+            
+    def _build_episode_input(
+        self,
+        relation_sampler: RelationSampler,
+        example_sampler: ExampleSampler,
+        entity_marker: EntityMarkerBuilder,
+        input_feature_builder: InputFeatureBuilder
+    ):
+        sampled_relation_list = relation_sampler.sample()
+        support_set, query_set = example_sampler.sample(relation=sampled_relation_list)
+        #添加rule_file
+        rule_file = "../../EXER/rule_o.csv"
+        prompt_s = PromptBuilder.build_support_prompt_inputs(
+                self,
+                token_list=support_set['token'],
+                head_list=support_set['head'],
+                tail_list=support_set['tail'],
+                relation_list=support_set['label'],
+                mask_token=self.tokenizer.mask_token,
+                pid2name=self.pid2name,
+                rule_file = rule_file
+        )
+        prompt_q = PromptBuilder.build_query_prompt_inputs(
+            self,
+            token_list=query_set['token'],
+            head_list=query_set['head'],
+            tail_list=query_set['tail'],
+            mask_token=self.tokenizer.mask_token,
+            rule_file = rule_file
+        )
+        
+        marker_s = entity_marker.mark(
+            token_list=support_set['token'],
+            head_idx_list=support_set['head_idx'],
+            tail_idx_list=support_set['tail_idx'],
+        )
+        marker_q = entity_marker.mark(
+            token_list=query_set['token'],
+            head_idx_list=query_set['head_idx'],
+            tail_idx_list=query_set['tail_idx'],
+        )
+        marker_s_text = [' '.join(m) for m in marker_s]
+        marker_q_text = [' '.join(m) for m in marker_q]
+        
+        prompt_input_s = input_feature_builder.build_input_feature(
+            tokenizer=self.tokenizer,
+            text_list=prompt_s,
+        )
+        prompt_input_q = input_feature_builder.build_input_feature(
+            tokenizer=self.tokenizer,
+            text_list=prompt_q,
+        )
+        marker_input_s = input_feature_builder.build_input_feature(
+            tokenizer=self.tokenizer,
+            text_list=marker_s_text,
+        )
+        marker_input_q = input_feature_builder.build_input_feature(
+            tokenizer=self.tokenizer,
+            text_list=marker_q_text,
+        )
+        
+        return {
+            'prompt_input': {
+                'support': prompt_input_s, 
+                'query': prompt_input_q
+            }, 
+            'marker_input': {
+                'support': marker_input_s, 
+                'query': marker_input_q
+            },
+            'sampled_relation': sampled_relation_list,
+            'sampled_support_set': support_set,
+            'sampled_query_set': query_set
+        }
+    
+    def train_loop(
+        self,
+        entity_marker,
+        input_feature_builder,
+        model,
+        loss_fn,
+        optimizer,
+    ):
+        model.train()
+        scaler = torch.cuda.amp.GradScaler()
+        for t in range(self.batch):
+            train_input = self._build_episode_input(
+                relation_sampler=self.rs_train,
+                example_sampler=self.es_train,
+                entity_marker=entity_marker,
+                input_feature_builder=input_feature_builder
+            )
+            with torch.cuda.amp.autocast():
+                preds = model(
+                    support_prompt_inputs=train_input['prompt_input']['support'],
+                    query_prompt_inputs=train_input['prompt_input']['query'],
+                    support_entity_marker_inputs=train_input['marker_input']['support'],
+                    query_entity_marker_inputs=train_input['marker_input']['query'],
+                    mask_token_id=self.tokenizer.mask_token_id, 
+                    head_start_token_id=self.special_token_ids["<subj>"],
+                    head_end_token_id=self.special_token_ids["</subj>"],
+                    tail_start_token_id=self.special_token_ids["<obj>"],
+                    tail_end_token_id=self.special_token_ids["</obj>"],
+                    K=self.train_K
+                )
+                targets = torch.tensor(
+                    self.label_mapping(
+                        sampled_relation=train_input['sampled_relation'],
+                        targets=train_input['sampled_query_set']['label']
+                    )
+                )
+                loss = loss_fn(preds, targets)
+            scaler.scale(loss).backward()
+            scaler.step(optimizer)
+            scaler.update()
+            optimizer.zero_grad()
+            if t % 10 == 0:
+                acc = self.get_acc(
+                    preds=preds,
+                    targets=targets,
+                    N=self.train_N,
+                    q=self.train_q
+                )
+                self.tprint(f"\tBatch:{t} / {self.batch}, Loss: {loss.item():.7f}, Acc: {acc:.5f}")
+    
+    def label_mapping(self, sampled_relation, targets):
+        label_mapping = {c:i for i, c in enumerate(sampled_relation)}
+        return [label_mapping[l] for l in targets]
+        
+    def get_acc(self, preds, targets, N, q):
+        correct = (preds.argmax(1) == targets).type(torch.float).sum().item()
+        acc = correct / (N * q)
+        return acc
+    
+    def tprint(self, print_str: str):
+        time_str = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+        print(time_str + '\t' + print_str)
